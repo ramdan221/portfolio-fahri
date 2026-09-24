94b7a149aef377c7b154200e7477e8529c394711
ramdan221 <ramadhanfah009@gmail.com>
2026-09-24 09:50:09 +0700

    Add files via upload

---
+++ perpus_fahri/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_perpustakaan_fahri";
+$koneksi = mysqli_connect($host, $user, $pass, $db);
+if(! $koneksi)   {die("koneksi ke database gagal:". 
+mysqli_connect_error());
+
+}
+?>
